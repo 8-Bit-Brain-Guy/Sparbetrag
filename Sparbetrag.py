206882fecfe134836b8366fb9e350c7ccff50cea
Tim Baumgarten <tbaumgarten@dspace.de>
2024-09-17 14:16:13 +0200

    1

--- Sparbetrag.py
+++ Sparbetrag.py
@@ -1,17 +1,36 @@
-# Parameter
+import os
+
+### Parameter
 Endbetrag = 0
 Startbetrag = 0
 Sparbetrag_mtl = 250.0
 Laufzeit = 120
-zinsen = 0.05
-zinsen_mtl = (zinsen / 12)
+Zinsen = 0.05
+Zinsen_mtl = (Zinsen / 12)
 
+### Berechnung
 Endbetrag = Startbetrag
-for i in range(Laufzeit+0):
-    Endbetrag = Endbetrag + (Endbetrag * zinsen_mtl) + Sparbetrag_mtl
+Monatsbetraege = []
+for i in range(Laufzeit+1):    
+    Endbetrag = Endbetrag + (Endbetrag * Zinsen_mtl) + Sparbetrag_mtl
+    Monatsbetraege.append(Endbetrag)
+
+
+
+### Analyse
+os.system('cls')
+print("########## Übersicht ##########")
+print("Parameter: ")
+print("Startbetrag:     %.2f" %Startbetrag)
+print("Sparbetrag_mtl:  %.2f" %Sparbetrag_mtl)
+print("Laufzeit:        %d" %Laufzeit)
+print("Zinsen:          %.2f" %Zinsen)
+print("\n")
+print("Analyse:")
+print("Monat        Betrag aktuell:")
+for i in range(len(Monatsbetraege)):    
     if (i % 12 == 0):
-        print("Betrag aktuell: %f" %Endbetrag)
+        print("%d            %.2f" %(i, Monatsbetraege[i]))
 
-print("Endbetrag: %f" %Endbetrag)
-
-
+print("-------------------------------")
+print("Endbetrag:     %.2f\n" %Endbetrag)
