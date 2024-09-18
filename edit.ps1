5fb0bc39e1332c04ba0fb847cacb48641b938128
Tim Baumgarten <tbaumgarten@dspace.de>
2024-09-18 12:02:59 +0200

    Edit Skript funktioniert jetzt

--- edit.ps1
+++ edit.ps1
@@ -1,5 +1,8 @@
-function notep([string]$a)
-{
-    write-host $a
-    return "Test"
-}
+# An das Sktipt übergeneme Parameter
+param([string]$fileToOpen)
+
+# Pfad zu Notepad++
+$notepadPlusPlusPath = "C:\Program Files\Notepad++\notepad++.exe"
+
+# Starten von Notepad++ mit Datei die geöffnet werden soll
+Start-Process -FilePath $notepadPlusPlusPath -ArgumentList $fileToOpen
