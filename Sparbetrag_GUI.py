95f734c237d330c29bdcba22595a96991332e879
8-Bit-Brain-Guy <>
2024-09-26 08:58:03 +0200

    1

--- Sparbetrag_GUI.py
+++ Sparbetrag_GUI.py
@@ -46,7 +46,15 @@ def on_entry(event):
         EndbetragEntry.insert(0, round(EndbetragValue, 2))
 
         ### Textfeld mit Verlauf der Gesamtsumme füllen
-        Textfeld.insert(END, "Test\n")
+        #Textfeld.delete(0, tk.END)
+        Textfeld.insert(END, "Monat     Wert\n")
+        Zwischenwert = Startbetrag
+        Monatsbetraege = []
+        for i in range(1, LaufzeitValue+1):
+            Zwischenwert = Zwischenwert + (Zwischenwert * Zinsen_mtl) + Sparbetrag_mtl
+            Monatsbetraege.append(Zwischenwert)
+            if ((i % 12 == 0) or (i in range(1, 12)) ):
+                Textfeld.insert(END, "%s         %s\n" % (i, round(Zwischenwert, 2)))
 
     except:
         EndbetragEntry.delete(0, tk.END)
@@ -64,7 +72,7 @@ root = tk.Tk()
 root.geometry("400x330")
 root.title("Sparbetrag")
 
-# Zwei Frames benutzen: Links Eingabefelder und Ausgabe für Endbetrag; Rechts detailierte Übersicht des Anwachsens des Entbetrages.
+### Zwei Frames benutzen: Links Eingabefelder und Ausgabe für Endbetrag; Rechts detailierte Übersicht des Anwachsens des Entbetrages.
 FrameLinks = tk.Frame(root)
 FrameLinks.pack()
 FrameLinks.pack(side = tk.LEFT)
@@ -73,7 +81,7 @@ FrameRechts = tk.Frame(root)
 FrameRechts.pack()
 FrameRechts.pack(side = tk.RIGHT)
 
-# Eingabefelder erzeugen
+### Eingabefelder erzeugen
 StartbetragEntry = tk.Entry(FrameLinks)
 Sparbetrag_mtlEntry = tk.Entry(FrameLinks)
 LaufzeitEntry = tk.Entry(FrameLinks)
@@ -81,11 +89,11 @@ ZinsenEntry = tk.Entry(FrameLinks)
 EndbetragEntry = tk.Entry(FrameLinks)
 
 
-# Textfeld erzeugen:
+### Textfeld erzeugen:
 Textfeld = tk.Text(master=FrameRechts, width=39, height=15, wrap='word')
 
 
-# Create labels for the entry fields
+### Create labels for the entry fields
 label1 = tk.Label(FrameLinks, text="Startbetrag", anchor="e")
 label2 = tk.Label(FrameLinks, text="Sparbetrag (mtl)")
 label3 = tk.Label(FrameLinks, text="Laufzeit (Monate)")
@@ -95,14 +103,14 @@ label6 = tk.Label(FrameRechts, text="Details")
 
 
 
-# Insert default values into the entry fields
+### Insert default values into the entry fields
 StartbetragEntry.insert(0, "0.0")
 Sparbetrag_mtlEntry.insert(0, "250.0")
 LaufzeitEntry.insert(0, "120")
 ZinsenEntry.insert(0, "0.05")
 EndbetragEntry.insert(0, "")
 
-# Bind the first entry field to capture 'Tab' or 'Enter' key events
+### Bind the first entry field to capture 'Tab' or 'Enter' key events
 StartbetragEntry.bind("<Tab>", on_entry)
 Sparbetrag_mtlEntry.bind("<Tab>", on_entry)
 LaufzeitEntry.bind("<Tab>", on_entry)
@@ -116,13 +124,13 @@ ZinsenEntry.bind("<Return>", on_entry)
 
 
 
-# Bind the ESC key to close the window
+### Bind the ESC key to close the window
 root.bind("<Escape>", close_window)
 
 
 
 
-# Pack the labels and entry fields into the window
+### Pack the labels and entry fields into the window
 label1.pack(padx=10, pady=5)
 StartbetragEntry.pack(padx=10, pady=5)
 label2.pack(padx=10, pady=5)
@@ -136,7 +144,7 @@ EndbetragEntry.pack(padx=10, pady=5)
 label6.pack(side = tk.TOP, padx=10, pady=5)
 Textfeld.pack(side = tk.BOTTOM, padx=1, pady=5)
 
-# Start the Tkinter event loop
+### Start the Tkinter event loop
 os.system('cls')
 root.mainloop()
 
